a2a751ba825a1038688408473b309e5f6cbf8424
George Aleksandria <giorgi.alexandria@gmail.com>
2017-07-02 16:53:43 +0300

    Add script to merge one repo to another

---
+++ git-merge-repo-repo.sh
@@ -0,0 +1,32 @@
+# merge one repository to another repository
+
+#$1 git path server `from` repositoy
+#$2 `from` repository name
+#$3 local path `to` repository
+#[$4] `from` branch by default `master`
+#[$5] `to` branch by default `master`
+
+fromBranch=${4:-master}
+toBranch=${5:-master}
+
+curDir=$(pwd)
+cd "$3"
+
+git remote add -f tempRepo $3$2.git
+git checkout -b tempBranch --no-track tempRepo/fromBranch
+
+git filter-branch --index-filter 'git ls-files -s | sed "s-\t-&'$2'/-" |
+GIT_INDEX_FILE=$GIT_INDEX_FILE.new \
+git update-index --index-info &&
+mv $GIT_INDEX_FILE.new $GIT_INDEX_FILE' HEAD
+
+git update-ref -d refs/original/refs/heads/tempBranch
+git remote rm tempRepo
+
+git checkout toBranch            
+git merge tempBranch --allow-unrelated-histories -m "Merge repository $2"
+git branch -D tempBranch
+
+#git push origin toBranch
+
+cd $curDir
