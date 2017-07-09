ee2a7ac2ee6d23eb9edf343fa293d69b3976c05f
George Aleksandria <giorgi.alexandria@gmail.com>
2017-07-09 21:53:57 +0300

    Fix git server path. Delete a ref to temporary branch if exists.

--- git-merge-repo-repo.sh
+++ git-merge-repo-repo.sh
@@ -12,8 +12,11 @@ toBranch=${5:-master}
 curDir=$(pwd)
 cd "$3"
 
-git remote add -f tempRepo $3$2.git
+git remote add -f tempRepo $1$2.git
 git checkout -b tempBranch --no-track tempRepo/fromBranch
+
+# Remove ref if exists
+git update-ref -d refs/original/refs/heads/tempBranch
 
 git filter-branch --index-filter 'git ls-files -s | sed "s-\t-&'$2'/-" |
 GIT_INDEX_FILE=$GIT_INDEX_FILE.new \
